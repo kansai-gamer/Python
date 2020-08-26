76bc3d7a814c7fbe3927c89332241943c94af52d
kansai-gamer <2004230001at@edu.oca.ac.jp>
2020-08-26 13:42:31 +0900

    0826

---
+++ Python0826/if4.py
@@ -0,0 +1,11 @@
+# math変数に85を代入している
+math = 85
+# eng変数に90を代入している
+eng = 90
+
+#条件 変数 math と eng が80以上だった場合処理をする(と思う)
+if math >= 80 and eng >= 80: # ここよく書き間違える
+
+    print("Good")
+else:
+    print("Bad")
