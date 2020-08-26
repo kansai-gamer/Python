507c3d999cc1b6dd9804ce4fde6fb65f1cecf8f6
kansai-gamer <2004230001at@edu.oca.ac.jp>
2020-08-26 12:56:51 +0900

    0826

--- Python0826/loop.py
+++ Python0826/loop.py
@@ -1,7 +1,9 @@
 i = 1
 while i <= 10:
-    if i <= 5:
+    if i % 3 == 1:
         print("A")
+    elif i % 3 == 2:
+        print("B")
     else:
-            print(i)
+        print("C")
     i += 1
