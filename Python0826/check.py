6a0c4e6b27a1434c162f036743c5ef76e76e6686
kansai-gamer <2004230001at@edu.oca.ac.jp>
2020-08-26 11:51:59 +0900

    0826

--- Python0826/check.py
+++ Python0826/check.py
@@ -1,5 +1,7 @@
-age = 1
+age = int(input("Type your age."))
 if age >= 20:
     print("OK")
+elif age >= 10:
+    print("Y E S")
 else:
     print("nope")
---
+++ Python0826/loop.py
@@ -0,0 +1,7 @@
+i = 1
+while i <= 10:
+    if i <= 5:
+        print("A")
+    else:
+            print(i)
+    i += 1
