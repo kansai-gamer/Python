d69cf87ad92a034aaef00fab74e865d609495056
kansai-gamer <2004230001at@edu.oca.ac.jp>
2020-09-02 13:19:00 +0900

    0902

--- Python0902/sample.py
+++ Python0902/sample.py
@@ -1,14 +1,7 @@
-# score = 80
-# score2 = 70
-# score3 = 90
-
-# total = score + score2 + score3
-# average = total / 3
-# print(average)
-scores = [80, 70, 90, 100, 50, 90, 70, 50, 60, 40, 11, 23, 45]
 i = 0
 total = 0
-while i < len(scores):
+scores = [80, 70, 90]
+while i <= 2:
     total = total + scores[i]
     i += 1
 print(total)
