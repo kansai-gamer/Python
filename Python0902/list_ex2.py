5cf791f46f673245fdf8a0d4d2843cd64a81c49e
kansai-gamer <2004230001at@edu.oca.ac.jp>
2020-09-02 14:20:03 +0900

    0902

---
+++ Python0902/list_ex2.py
@@ -0,0 +1,3 @@
+ages = [22, 20, 25, 20, 18]
+for i in range(len(ages)):
+    print(ages[i])
---
+++ Python0902/list_ex7.py
@@ -0,0 +1,7 @@
+ages = [22, 20, 25, 20, 18]
+
+max = 0
+for age in ages:
+    if ages[0] < age:
+        max = age
+print(max)
