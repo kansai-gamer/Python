e357ae78856de847fa460ebda2406a8e108a523a
kansai-gamer <2004230001at@edu.oca.ac.jp>
2020-09-02 16:15:08 +0900

    0902

--- Python0902/wh3.py
+++ Python0902/wh3.py
@@ -1,11 +1,10 @@
 #not
 i = 1
 while i <= 9:
-    j = 1
-        if j == 5:
-            continue
-    while j <= 9:
-        print(i * j, end=" ")
-        j += 1
-    print()
+    if i != 5:
+        j = 1
+        while j <= 9:
+            print(i * j, end=" ")
+            j += 1
+        print()
     i += 1
