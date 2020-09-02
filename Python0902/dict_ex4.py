54787897488ac79ee9ee0ffd6a36406aa6e39e92
kansai-gamer <2004230001at@edu.oca.ac.jp>
2020-09-02 16:07:21 +0900

    0902

---
+++ Python0902/dict_ex4.py
@@ -0,0 +1,2 @@
+fruits = {"Apple": 100, "Bnana": 200, "Cherry": 300}
+print(len(fruits))
---
+++ Python0902/dict_ex5.py
@@ -0,0 +1,5 @@
+fruits = {"Apple": 100, "Banana": 200, "Cherry": 300}
+total = 0
+for key in fruits.values():
+    total = total + key
+print(total)
