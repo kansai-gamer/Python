c8d700664694925b9bd53bf75690452fd4ffa9fc
kansai-gamer <takumi@tutanota.de>
2020-09-09 15:11:12 +0900

    0909

--- Python0909/lc1_2.py
+++ Python0909/lc1_2.py
@@ -2,3 +2,10 @@ numbers = []
 for i in range(10):
     numbers.append(i)
 print(numbers)
+
+numbers = [1, 2, 3, 4, 5]
+numbers2 = []
+for i in numbers:
+    if i % 2 == 0:
+        numbers2.append(i)
+print(numbers2)
