a35134a5691a1713d802f2f1c3a59a75cae761dc
kansai-gamer <takumi@tutanota.de>
2020-09-16 15:52:30 +0900

    0916

---
+++ Python0916/f2.py
@@ -0,0 +1,31 @@
+a = 30
+b = 3
+
+#足し算関数
+def add_nums(x, y):
+    add = x + y
+    return add
+#結果
+print("足し算", add_nums(a, b))
+
+#引き算関数
+def minus_nums(x, y):
+    minus = x - y
+    return minus
+#結果
+print("引き算", minus_nums(a, b))
+
+#掛け算関数
+def multiple_nums(x, y):
+    multiple = x * y
+    return multiple
+
+#結果
+print("掛け算", minus_nums(a, b))
+
+#割り算関数
+def division(x, y):
+    division = x / y
+    return division
+#結果
+print("割り算", division(a, b))
