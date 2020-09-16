f65a092e9a46376b67f6163d70d49aa333094e67
kansai-gamer <takumi@tutanota.de>
2020-09-16 15:36:19 +0900

    0916

--- Python0916/f.py
+++ Python0916/f.py
@@ -1,12 +1,12 @@
-def setoverages(users, setage):
+def set_over_ages(users, age):
     result = []
     for name, age in users.items():
-        if age >= setage:
+        if age >= age:
             result.append(name)
     return result
 
 
 users = {"murayama": 41, "tanaka": 21, "yoshida": 18}
-result = setoverages(users, 18)
+result = set_over_ages(users, 18)
 for name in result:
     print(name)
