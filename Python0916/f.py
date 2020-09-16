05f58cce6536b82615b9b1d5c65c7b1139ab7521
kansai-gamer <takumi@tutanota.de>
2020-09-16 15:26:48 +0900

    0916

---
+++ Python0916/f.py
@@ -0,0 +1,14 @@
+def over20ages(users):
+    result = []
+    for name, age in users.items():
+        if age >= 20:
+            result.append(name)
+    return result
+    
+
+
+
+users = {"murayama": 41, "tanaka": 21, "nakamura": 18}
+result = over20ages(users)
+for name in result:
+    print(name)
--- Python0916/func_ex5.py
+++ Python0916/func_ex5.py
@@ -1,2 +1,4 @@
+randomnums = random.randint(0,3)
 nums = input("Please enter a number (0:Goo, 1:Choki, 2:Par) ")
-if int(nums)
+if int(nums) == randomnums:
+    
