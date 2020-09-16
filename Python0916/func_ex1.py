8f1238969e4a46fd199104c4f3e804d2c8016484
kansai-gamer <takumi@tutanota.de>
2020-09-16 13:44:32 +0900

    0916

---
+++ Python0916/func_ex1.py
@@ -0,0 +1,3 @@
+numbers = [10, -20, 30, -40, 50]
+for i in range(0,len(numbers)):
+    print(abs(numbers[i]))
---
+++ Python0916/func_ex2.py
@@ -0,0 +1,8 @@
+table = [
+  [10, 20, 30],
+  [40, 50, 60],
+  [70, 80, 90],
+]
+print(max(table[0]))
+print(max(table[1]))
+print(max(table[2]))
