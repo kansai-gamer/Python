e615425c4654f7e7c0089d4791c6d6565e5843cd
kansai-gamer <takumi@tutanota.de>
2020-09-16 14:02:34 +0900

    0916

---
+++ Python0916/func_ex3.py
@@ -0,0 +1,5 @@
+total = 0
+points = ["10", "20", "30"]
+for i in range(0,len(points)):
+    total = total + int(points[i])
+print(total)
---
+++ Python0916/func_ex4.py
@@ -0,0 +1,5 @@
+nums = input("Please enter a number ")
+if int(nums) % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
