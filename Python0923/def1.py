db02b1597e26e8391eeab37ee437bcf1eab6f309
kansai-gamer <takumi@tutanota.de>
2020-09-23 11:44:36 +0900

    0923

--- Python0923/def1.py
+++ Python0923/def1.py
@@ -2,7 +2,7 @@ def add(x, y):
     z = x + y
     return z
 
-a = add(10, 20)
-b = add(30, 40)
-c = add(a, b)
-print(c)
+# a = add(10, 20)
+# b = add(30, 40)
+# c = add(a, b)
+print(add(add(30, 40), add(10, 20)))
