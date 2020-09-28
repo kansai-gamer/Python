2e5f65806cf94065623a3ec35db65c6246ef93ce
kansai-gamer <takumi@tutanota.de>
2020-09-28 15:15:28 +0900

    0928

--- Python0916/func_ex5.py
+++ Python0916/func_ex5.py
@@ -1,6 +1,16 @@
 from random import randint
-randomnums = randint(0,3)
-print(randomnums)
-nums = input("Please enter a number (0:Goo, 1:Choki, 2:Par) ")
-if nums == randomnums:
+
+randomnums = randint(0,2)
+
+user = int(input("Please enter a number (0:Goo, 1:Choki, 2:Par) "))
+
+if user == (randomnums - 1) % 3:
+    print("Win")
+
+elif user == (randomnums + 1) % 3:
+    print("lose")
+
+else:
     print("Draw")
+
+#松岡秀真さんのコードをパク...参考にしました。
