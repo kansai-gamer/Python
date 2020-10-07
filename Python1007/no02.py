d87337c266f49d62e35054c718c56125ba40265e
kansai-gamer <me@japan-is.fun>
2020-10-07 14:00:52 +0900

    1007

---
+++ Python1007/no02.py
@@ -0,0 +1,7 @@
+count = 3
+message = "Hello"
+
+j = 0
+for i in range(count):
+    j = j + 1
+    print(j, ":", message)
---
+++ Python1007/no03.py
@@ -0,0 +1,6 @@
+count = 10
+message1= "Hello"
+message2 = "World"
+
+for i in range(count):
+    print(message1)
