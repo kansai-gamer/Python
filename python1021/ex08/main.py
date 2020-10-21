4cd07868eb9e4ec0895c35364fe29485258add13
kansai-gamer <me@japan-is.fun>
2020-10-21 15:56:53 +0900

    1021

---
+++ python1021/ex08/main.py
@@ -0,0 +1,8 @@
+from mysales.sale import sale
+
+sale = sale(800,10)
+sale.price = 1000
+sale.count = 20
+sale.print()
+print(sale.price)
+print(sale.count)
---
+++ python1021/ex08/mysales/sale.py
@@ -0,0 +1,10 @@
+class sale:
+    def __init__(self, price, count):
+        self.price = price
+        self.count = count
+
+    def calc(self):
+        return self.price * self.count
+
+    def print(self):
+        print(f"{self.calc():,d}")
