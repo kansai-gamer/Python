99f20db5b073df490b8786a3c1067e1830668420
kansai-gamer <me@japan-is.fun>
2020-10-21 14:06:28 +0900

    1021

--- python1021/ex03/mysales/sale.py
+++ python1021/ex03/mysales/sale.py
@@ -3,4 +3,13 @@ class Sale:
         self.price = 800
         self.count = 10
 
-    
+    def calc(self):
+        kotae = self.price * self.count
+        return kotae
+
+    def print(self):
+        kekka = self.price * self.count
+                    #↓カンマを入れる処理&出力
+        return print("{:,}".format(kekka))
+
+#参考元 https://www.yoheim.net/blog.php?q=20160404
