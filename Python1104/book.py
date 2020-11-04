71786988f834eb4d88d72271b132bf3f7de748d8
kansai-gamer <me@japan-is.fun>
2020-11-04 11:41:00 +0900

    1104

--- Python1104/book.py
+++ Python1104/book.py
@@ -1,2 +1,21 @@
 class Book:
-    pass
+    def __init__(self, title, price,peges):
+        self.title = title
+        self.price = price
+        self.peges = peges
+        
+    def show(self):
+        print(self.title, self.price, self.peges)
+
+class category:
+    def name(self):
+        
+
+
+book1 = Book("python book", 3000, 300)
+book2 = Book("PHP book", 2000, 250)
+book3 = Book("Hello english", 1000, 200)
+
+book1.show()
+book2.show()
+book3.show()
