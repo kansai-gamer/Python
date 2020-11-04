435da4409c24933f28c9e5157246382dbb46d787
kansai-gamer <me@japan-is.fun>
2020-11-04 13:00:38 +0900

    1104

--- Python1104/book.py
+++ Python1104/book.py
@@ -8,14 +8,29 @@ class Book:
         print(self.title, self.price, self.peges)
 
 class category:
-    def name(self):
-        
+    def __init__(self, name):
+        self.name = name
+        self.books = []
 
+    def show(self):
+        print(self.name)
+        for book in self.books:
+            book.show()
+
+    def add_book(self, book):
+        self.books.append(book)
+
+category1 = category("programming")
+category2 = category("english")
 
 book1 = Book("python book", 3000, 300)
 book2 = Book("PHP book", 2000, 250)
 book3 = Book("Hello english", 1000, 200)
 
-book1.show()
-book2.show()
-book3.show()
+category1.add_book(book1)
+category1.add_book(book2)
+
+category1.show()
+
+category2.add_book(book3)
+category2.show()
