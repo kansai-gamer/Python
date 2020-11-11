168e9e9cb0e434e731c93e13401d414bf7d939df
kansai-gamer <me@japan-is.fun>
2020-11-11 16:16:57 +0900

    1111

--- python1111/score.py
+++ python1111/score.py
@@ -1,12 +1,11 @@
 f = open("score.csv", mode="r")
 score = f.read()
 f.close()
-print(score)
 
 date = score.split(",")
 
 date_int = [int(s) for s in date]
 
 f = open("result.txt", mode="w")
-f.write(date_int)
+f.write(str(sum(date_int)))
 f.close()
