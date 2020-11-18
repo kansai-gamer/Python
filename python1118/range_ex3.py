97e2407d0212dca4388a0600882bb156fbd6c293
kansai-gamer <me@japan-is.fun>
2020-11-18 16:08:54 +0900

    1118

--- python1118/range_ex3.py
+++ python1118/range_ex3.py
@@ -1,5 +1,9 @@
 def odd_generator(start = 1, stop = 10):
-    
+    index = start
+    while index < stop:
+        if index % 2 == 1:
+            yield index 
+        index += 1
 
 
 og = odd_generator()
