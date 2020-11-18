3c4a4f32d35b898bd9a3d0f7ee836e4062bead3b
kansai-gamer <me@japan-is.fun>
2020-11-18 11:58:42 +0900

    1118

---
+++ python1118/1118.py
@@ -0,0 +1,17 @@
+names = ["tanaka", "saito", "suzuki"]
+
+names.insert(1, "maeda")
+
+names.reverse()
+
+upper = list(map(str.upper,names))
+
+print(upper)
+
+# upper = []
+
+# for name in names:
+#     upper.append(name.upper())
+# print(upper)
+
+#参考元 https://qiita.com/sino20023/items/c5f56386e6e03d6d078d
