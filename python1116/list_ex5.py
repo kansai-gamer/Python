cdd0f2c1f599d788406dba7f42fda3ab2926c962
kansai-gamer <me@japan-is.fun>
2020-11-18 11:13:38 +0900

    1116

---
+++ python1116/list_ex5.py
@@ -0,0 +1,10 @@
+names1 = ["Alice", "Andy", "Bob", "Carol"]
+names2 = ["Alice", "Betty", "Carol", "Charlie"]
+
+tasu = names1 + names2
+
+tasu.sort
+
+print(sorted(set(tasu)))
+
+#参考元 https://note.nkmk.me/python-list-unique-duplicate/
---
+++ python1116/list_ex6.py
@@ -0,0 +1,4 @@
+numbers = [1, 2, 3, 4, 5]
+while len(numbers) > 0:
+    numbers = number
+    print(number)
