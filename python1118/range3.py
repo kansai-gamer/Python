ced038e0692170c295e50099f76309f13a1d3071
kansai-gamer <me@japan-is.fun>
2020-11-18 13:41:14 +0900

    1118

--- python1118/range3.py
+++ python1118/range3.py
@@ -5,11 +5,20 @@ class MyRange:
         self.step = step
 
     def __iter__(self):
-        return self
+        print("__iter__")
+        return Myiterator(self.index, self.stop, self.step)
+
+class Myiterator:
+    def __init__(self, index, stop, step):
+        self.index = index
+        self.stop = stop
+        self.step = step
 
     def __next__(self):
+        print("__next__")
         if self.index == self.stop:
             raise StopIteration()
+
         value = self.index
         self.index += 1
         return value
