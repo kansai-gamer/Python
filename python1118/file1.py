b8a11413348de89241f9b086164a61dbf3cc0805
kansai-gamer <me@japan-is.fun>
2020-11-18 15:57:47 +0900

    1118

--- python1118/file1.py
+++ python1118/file1.py
@@ -1,11 +1,8 @@
 names = ["Alice", "Bob", "Charlie", "Dave"]
 
-
-
 f = open("names.txt", mode="w")
 
-i = 1
-for name in names:
-    i += 1
-    f.write(str(i) + "," + name + "\n")
+for i, name in enumerate(names):
+    f.write(str(i + 1) + "," + name + "\n")
+
 f.close()
