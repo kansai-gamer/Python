663bd9fc978d1498fd88e7d115f0b132f730e164
kansai-gamer <me@japan-is.fun>
2020-11-25 11:12:48 +0900

    1125

---
+++ Python1125/alphabet.py
@@ -0,0 +1,10 @@
+class Alphabet:
+    def __init__(self):
+        self.alphabet = "ABCDEFGHIJKMNLOPQRSTUVWXYZ"
+
+    def show(self):
+        print(self.alphabet)
+
+
+alphabet = Alphabet()
+alphabet.show()
