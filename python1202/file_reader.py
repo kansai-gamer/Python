4fb926b03b0bc57ed5faf6bf4c1e4e521623f1c5
kansai-gamer <me@japan-is.fun>
2020-12-02 11:38:42 +0900

    1202

--- python1202/file_reader.py
+++ python1202/file_reader.py
@@ -3,6 +3,9 @@ file_name = "score.txt"
 with open(file_name, 'r') as f:
     r = f.read()
 
-total = r.split()
+l = r.split(" ")
+print(r)
+
+total = sum([int(e) for e in l])
+
 print(total)
-print(sum(total))
