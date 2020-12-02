9f19f0a62f9cd38eb8f2262cbf3c8d8256d361d7
kansai-gamer <me@japan-is.fun>
2020-12-02 16:10:57 +0900

    1202

--- python1202/ex_ex5.py
+++ python1202/ex_ex5.py
@@ -1,17 +1,18 @@
-class MyNotFoundError(targets, fruits):
+class MyNotFoundError(Exception):
     pass
 
-def search(target, fruits):
-    if target == fruits:
-        return "Found"
-    else:
-        return "NotFound"
+def search(fruits, target):
+    while True:
+        if target in fruits:
+            return f"{target} is found."
+        else:
+            raise MyNotFoundError(f"{target} is not found.")
 
 fruits = ["Lemon", "Orange", "Grape", "Banana", "Peach"]
 targets = ["Orange", "Apple", "Lemon"]
 
 for target in targets:
     try:
-        print(search(fruits, targets))
+        print(search(fruits, target))
     except MyNotFoundError as e:
         print(e)
