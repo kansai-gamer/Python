914d1281e032028b081fad37c8452a3b80585fd2
kansai-gamer <me@japan-is.fun>
2020-12-09 16:00:26 +0900

    1209

--- python1209/task_app.py
+++ python1209/task_app.py
@@ -4,14 +4,17 @@ class Task:
     STATUS_CLOSED = 0
     STATUS_OPENED = 1
 
-    def __init__(self, id, title, data, status):
+    def __init__(self, id, title, date, status):
         self.id = id
         self.title = title
-        self.data = data
+        self.date = date
         self.status = status
 
     def show(self):
-        print(self.id, self.title, self.data, self.status)
+        print(self.id, self.title, self.date, self.status)
+
+class TaskBoardError(Exception):
+    pass
 
 class TaskBoard:
     def __init__(self):
@@ -28,9 +31,38 @@ class TaskBoard:
         for task in self.task_list:
             task.show()
 
+    def edit_task(self, id, title, status):
+        for task in self.task_list:
+            edit_flag = False
+            if task.id == id:
+                task.title = title
+                task.date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                task.status = status
+                edit_flag = True
+
+        if edit_flag == False:
+            raise TaskBoardError()
+
+    def delete_task(self, id):
+        try:
+            self.task_list.pop(id)
+        except IndexError:
+            raise TaskBoardError()
+    
+    def save_file(self, file_name):
+        self.file_name = file_name
+        f = open(file_name,'w')
+        for task in self.task_list:
+            f.write(str(task.id)+ "," + str(task.title)+ "," + str(task.date) + "," + str(task.status)+ "\n")
+        f.close()
+
+    def load_file(self, file_name):
+        with open(file_name) as f:
+            s = f.readlines
+            
+        
+
 if __name__ == "__main__":
     task_board = TaskBoard()
-    task_board.add_task("Study Python")
-    task_board.add_task("Study PHP")
-    task_board.add_task("Clean the room")
+    task_board.load_file("task.csv")
     task_board.show_task_list()
